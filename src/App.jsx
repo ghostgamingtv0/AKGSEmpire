3232d95bb7be1335b95de7bac8bca36040e87d68
AKGS Developer <dev@akgsempire.org>
2026-02-22 23:58:40 +0000

    set_root_route_to_empire

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ComingSoon from './pages/ComingSoon/ComingSoon';
 import GhostGate from './pages/GhostGate/GhostGate';
 import Empire from './pages/Empire/Empire';
 
@@ -8,13 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* 1. Genesis Gate - Main Landing Page (Home) */}
-        <Route path="/" element={<GhostGate />} />
-        
-        {/* 2. Coming Soon - Independent Page */}
-        <Route path="/coming-soon" element={<ComingSoon />} />
-        
-        {/* 3. Main Site (AKGS Empire) - Isolated Module */}
+        <Route path="/" element={<Empire />} />
         <Route path="/empire/*" element={<Empire />} />
       </Routes>
     </Router>
