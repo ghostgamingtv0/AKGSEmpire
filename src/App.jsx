693bf9bc722d862e171272de7a8bbd380d8ab2a9
AKGS Developer <dev@akgsempire.org>
2026-02-23 02:54:35 +0000

    root_to_ghostgate_and_comingsoon_route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GhostGate from './pages/GhostGate/GhostGate';
+import ComingSoon from './pages/ComingSoon/ComingSoon';
 import Empire from './pages/Empire/Empire';
-import Login from './pages/Empire/components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<GhostGate />} />
+        <Route path="/coming-soon" element={<ComingSoon />} />
         <Route path="/empire/*" element={<Empire />} />
       </Routes>
     </Router>
